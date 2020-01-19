7946112570eff3cb92a55d793b199776564bc0ac
Nighthree <jenny605328@gmail.com>
2020-01-19 23:28:16 +0800

    創建開發分支

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-
+import Home from '@/components/Home';
 
 
 Vue.use(VueRouter);
 export default new VueRouter({
   linkActiveClass:'active',
-  routes:[],
+  routes:[
+    {
+      path: '*',
+      redirect: '/login',
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+  ],
 });
