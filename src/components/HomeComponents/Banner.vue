ba1c092636a89ba683fd21488d52fd2ab0ab7583
Nighthree <jenny60538@gmail.com>
2020-02-23 15:53:46 +0800

    調整Banner

--- src/components/HomeComponents/Banner.vue
+++ src/components/HomeComponents/Banner.vue
@@ -7,43 +7,46 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active" style="z-index:0">
-        <router-link to="/products" class="goProducts">立刻選購</router-link>
-        <img
-          src
-          class="d-block w-100 bg-cover bannerHeight"
-          alt
-          style="background-image:url(https://vanillaknight.com/media/pics/20200102/1577952582.jpg)"
-        />
-        <div class="carousel-caption bg-op6 p-2 p-md-4">
-          <h5>全館 8 折</h5>
-          <p class="carouselPSize mb-2">輸入折扣碼 888 即享購物車 8 折優惠</p>
-        </div>
+        <router-link to="/products" class title="立刻選購">
+          <img
+            src
+            class="d-block w-100 bg-cover bannerHeight"
+            alt
+            style="background-image:url(https://vanillaknight.com/media/pics/20200102/1577952582.jpg)"
+          />
+          <div class="carousel-caption bg-op6 p-2 p-md-4">
+            <h5>全館 8 折</h5>
+            <p class="carouselPSize mb-2">輸入折扣碼 888 即享購物車 8 折優惠</p>
+          </div>
+        </router-link>
       </div>
       <div class="carousel-item" style="z-index:0">
-        <router-link to="/products" class="goProducts">立刻選購</router-link>
-        <img
-          src
-          class="d-block w-100 bg-cover bannerHeight"
-          alt
-          style="background-image:url(https://vanillaknight.com/media/pics/20190814/1565775858.jpg)"
-        />
-        <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
-          <h5>全館 6 折</h5>
-          <p class="carouselPSize mb-2">輸入折扣碼 666 即享購物車 6 折優惠</p>
-        </div>
+        <router-link to="/products">
+          <img
+            src
+            class="d-block w-100 bg-cover bannerHeight"
+            alt
+            style="background-image:url(https://vanillaknight.com/media/pics/20190814/1565775858.jpg)"
+          />
+          <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
+            <h5>全館 6 折</h5>
+            <p class="carouselPSize mb-2">輸入折扣碼 666 即享購物車 6 折優惠</p>
+          </div>
+        </router-link>
       </div>
       <div class="carousel-item" style="z-index:0">
-        <router-link to="/products" class="goProducts">立刻選購</router-link>
-        <img
-          src
-          class="d-block w-100 bg-cover bannerHeight"
-          alt
-          style="background-image:url(https://vanillaknight.com/shop/pics/20180215/1518667878.jpg)"
-        />
-        <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
-          <h5>全館 6 折</h5>
-          <p class="carouselPSize mb-2">輸入折扣碼 666 即享購物車 6 折優惠</p>
-        </div>
+        <router-link to="/products">
+          <img
+            src
+            class="d-block w-100 bg-cover bannerHeight"
+            alt
+            style="background-image:url(https://vanillaknight.com/shop/pics/20180215/1518667878.jpg)"
+          />
+          <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
+            <h5>全館 6 折</h5>
+            <p class="carouselPSize mb-2">輸入折扣碼 666 即享購物車 6 折優惠</p>
+          </div>
+        </router-link>
       </div>
     </div>
     <a
